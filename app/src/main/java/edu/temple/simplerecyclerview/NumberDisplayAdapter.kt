4a2e7dd3e2e650df89d1238ce3e29eea351ed48f
heronziegel <tuo12440@temple.edu>
2023-09-26 15:05:43 -0400

    Implemented the NumberDisplayAdapter class with necessary functions and a customer NumberViewHolder

--- app/src/main/java/edu/temple/simplerecyclerview/NumberDisplayAdapter.kt
+++ app/src/main/java/edu/temple/simplerecyclerview/NumberDisplayAdapter.kt
@@ -6,21 +6,28 @@ import androidx.recyclerview.widget.RecyclerView
 
 // Step 3: Complete adapter implementation
 
-class NumberDisplayAdapter () : RecyclerView.Adapter<NumberDisplayAdapter.NumberViewHolder>() {
+class NumberDisplayAdapter (_numbers: Array<Int>) : RecyclerView.Adapter<NumberDisplayAdapter.NumberViewHolder>() {
 
-    class NumberViewHolder (val textView: TextView) : RecyclerView.ViewHolder (textView) {}
+    private val numbers = _numbers
+    class NumberViewHolder (val _textView: TextView) : RecyclerView.ViewHolder (_textView) {
+        val textView = _textView
+    }
 
     //Step 3b: Complete function definitions for adapter
     override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): NumberViewHolder {
-        TODO("Not yet implemented")
+        return NumberViewHolder(
+            TextView (
+                parent.context
+            )
+        )
     }
 
     override fun getItemCount(): Int {
-        TODO("Not yet implemented")
+        return numbers.size
     }
 
     override fun onBindViewHolder(holder: NumberViewHolder, position: Int) {
-        TODO("Not yet implemented")
+        holder.textView.setText(numbers[position])
     }
 
 
